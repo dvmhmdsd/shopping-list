fa961371ee93a1923cf56ef6121fb61b65d81484
mohamed-saad <mahameds19999@gmail.com>
2019-01-17 11:37:02 +0200

    save to the db

--- routes/api/items.js
+++ routes/api/items.js
@@ -4,7 +4,17 @@ const Item = require('../../models/Item');
 
 // get all items
 router.get('/', (req, res) => {
-    Item.find({}).sort({date: -1}).then(items => res.json(items));
+    Item.find({})
+        .sort({date: -1})
+        .then(items => res.json(items));
+});
+
+// post 
+router.post('/', (req, res) => {
+    const item = new Item({ name: req.body.name});
+
+    // save to db 
+    item.save().then(item => res.json(items));
 });
 
 module.exports = router;
