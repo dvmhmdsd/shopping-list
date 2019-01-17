aad94d80e5b12ad32d0b78b40bba39d20f263857
mohamed-saad <mahameds19999@gmail.com>
2019-01-17 11:51:46 +0200

    set the post and delete requests

--- routes/api/items.js
+++ routes/api/items.js
@@ -14,7 +14,12 @@ router.post('/', (req, res) => {
     const item = new Item({ name: req.body.name});
 
     // save to db 
-    item.save().then(item => res.json(items));
+    item.save().then(item => res.json(item));
+});
+
+// delete
+router.delete('/:id', (req, res) => {
+    Item.findByIdAndRemove(req.params.id).then(() => res.json({success: true})).catch(err => res.status(404).json({success: false}));
 });
 
 module.exports = router;
