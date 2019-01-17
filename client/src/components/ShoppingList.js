ca3a6d993cd0be1cdd832de1059b04f285222884
mohamed-saad <mahameds19999@gmail.com>
2019-01-18 00:07:33 +0200

    steps I didn't understand but it's for get items

--- client/src/components/ShoppingList.js
+++ client/src/components/ShoppingList.js
@@ -2,18 +2,19 @@ import React, { Component } from 'react';
 import { Container, ListGroup, ListGroupItem, Button } from 'reactstrap';
 import {  CSSTransition, TransitionGroup } from 'react-transition-group';
 import uuid from 'uuid';
+import { connect } from 'react-redux';
+import { getItems } from '../actions/itemActions';
+import PropTypes from 'prop-types';
+
 
 export class ShoppingList extends Component {
-  state = {
-      items: [
-          {id: uuid(), name: 'ali'},
-          {id: uuid(), name: 'amgad'},
-          {id: uuid(), name: 'nour'},
-      ]
-  }
-  
+
+    componentDidMount() {
+        this.props.getItems();
+    }
+
   render() {
-      const { items } = this.state;
+      const { items } = this.porps.item;
     return (
       <div>
           <Container>
@@ -60,4 +61,13 @@ export class ShoppingList extends Component {
   }
 }
 
-export default ShoppingList;
+ShoppingList.propTypes = {
+    getItems: PropTypes.func.isRequired,
+    item: PropTypes.object.isRequired
+}
+
+const mapStateToProps = (state) => {
+    item: state.item
+}
+
+export default connect(mapStateToProps, { getItems })(ShoppingList);
