931d77c4b90fc736b3db37dc2b79ed92228ae531
mohamed-saad <mahameds19999@gmail.com>
2019-01-18 22:15:26 +0200

    edit

--- index.js
+++ index.js
@@ -1,41 +1,45 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const config = require('./config/database');
-const path = require('path');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const config = require("./config/database");
+const path = require("path");
 
-const items = require('./routes/api/items');
+const items = require("./routes/api/items");
 
 const app = express();
 
 // body parser
 app.use(bodyParser.json());
 
-
 // mongoose
-mongoose.connect(config.mongoURI, { useNewUrlParser: true });
+mongoose.connect(
+  config.mongoURI,
+  { useNewUrlParser: true }
+);
 
 const db = mongoose.connection;
 
 // on connect
-db.on('connected', () => {
-    console.log('connected');
+db.on("connected", () => {
+  console.log("connected");
 });
 
 // on error
-db.on('error', (error) => {
-    console.log(error);
+db.on("error", error => {
+  console.log(error);
 });
 
-app.use(express.static('build'));
-// routes
-app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
-});
+if ((process.env.NODE_ENV = "production")) {
+  app.use(express.static("client/build"));
+  // routes
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
-app.use('/api/items', items);
+app.use("/api/items", items);
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    console.log(`server run on ${PORT}`);
+  console.log(`server run on ${PORT}`);
 });
