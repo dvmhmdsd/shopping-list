239e47e57b76dded91d2f5c8ddd85bdcc40af70c
mohamed-saad <mahameds19999@gmail.com>
2019-01-18 21:58:44 +0200

    finish the project

---
+++ build/precache-manifest.b01aab715e3e2c9570ddb1e288691486.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3b86c9c6700c51006309",
+    "url": "/static/js/main.3b86c9c6.chunk.js"
+  },
+  {
+    "revision": "a00c69e06bf74f85c89d",
+    "url": "/static/js/1.a00c69e0.chunk.js"
+  },
+  {
+    "revision": "3b86c9c6700c51006309",
+    "url": "/static/css/main.48e3df31.chunk.css"
+  },
+  {
+    "revision": "a00c69e06bf74f85c89d",
+    "url": "/static/css/1.855b9202.chunk.css"
+  },
+  {
+    "revision": "8fbcdc583eb8b8c1380a8d8cad4016ba",
+    "url": "/index.html"
+  }
+];
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const config = require('./config/database');
+const path = require('path');
 
 const items = require('./routes/api/items');
 
@@ -26,8 +27,12 @@ db.on('error', (error) => {
     console.log(error);
 });
 
-
+app.use(express.static('build'));
 // routes
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+});
+
 app.use('/api/items', items);
 
 const PORT = process.env.PORT || 5000
